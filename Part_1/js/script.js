28547bac700aab0499357f820ddf96a9f70a110b
Tatiana Liatambur <T.Goroshina@yandex.ru>
2017-04-01 01:15:27 +0300

    test request

--- Part_1/js/script.js
+++ Part_1/js/script.js
@@ -1,3 +1,23 @@
-/**
- * Created by TGoro on 31.03.2017.
- */
+'use strict';
+
+function GoogleCallback (func, data) {
+    window[func](data);
+}
+
+$ (function () {
+
+
+    $.ajax({
+        url:
+       'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=931360a1-2720-49e9-8dd2-8cd16dd37b42&q=PHP',
+        method: 'POST',
+        datatype: 'jsonp',
+        success: function () {
+
+        },
+        error: function () {
+
+        }
+    });
+
+});
