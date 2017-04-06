c44ecbc7f5158a4341c14d401eb30948684f6bb9
Tatiana Liatambur <T.Goroshina@yandex.ru>
2017-04-07 00:01:08 +0300

    Ready part_1

--- Part_1/js/script.js
+++ Part_1/js/script.js
@@ -1,23 +1,56 @@
 'use strict';
 
-function GoogleCallback (func, data) {
-    window[func](data);
+function getText() {
+    var inputText = $('.search-form_input');
+    return inputText[0].value;
 }
 
-$ (function () {
+function runSearch () {
 
+    $('.post').remove();
+
+    var searchText = getText();
+    var url = 'http://webhose.io/search?token=931360a1-2720-49e9-8dd2-8cd16dd37b42&format=json&q=' + searchText + '%20language%3A(english)';
 
     $.ajax({
-        url:
-       'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=931360a1-2720-49e9-8dd2-8cd16dd37b42&q=PHP',
+        url: url,
         method: 'POST',
-        datatype: 'jsonp',
-        success: function () {
+        success: function (serverResponse) {
 
+            var resultWrapper = $('.showResult');
+
+            for (var i = 0; i < serverResponse.posts.length; i++) {
+
+                var post = serverResponse.posts[i];
+
+                var $post = $('<div>').addClass('post');
+                var $title = $('<a>')
+                    .addClass('post_title')
+                    .attr('href', post.url)
+                    .text(post.title || post.text.substring(0, post.text.indexOf('.')));
+                var $url = $('<p>')
+                    .addClass('post_url')
+                    .text(post.url.substring(0, 70));
+                var $text = $('<p>')
+                    .addClass('post_text')
+                    .text(post.text.substring(0, post.text.indexOf('.', 200)));
+                $post.append($title).append($url).append($text);
+                resultWrapper.append($post);
+            }
         },
         error: function () {
 
         }
     });
+}
 
+$ (function () {
+
+    var $clickButton = $('.search-form_button');
+    var $input = $('.search-form_input');
+    $clickButton.on('click', runSearch);
+    $input.on('keypress', function (event) {
+        if (event.keyCode === 13)
+            runSearch();
+    });
 });
